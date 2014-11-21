682746a8f5f7a5c089a7c78a28b0afe9d13364df
Soubhagya Dash <Soubhagya.Dash@microsoft.com>
2014-11-21 15:57:34 -0800

    init

---
+++ MVC2App/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC2App.Models;
+
+namespace MVC2App.Controllers
+{
+    [AiHandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to ASP.NET MVC!";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var r = new Random();
+            if (r.Next() % 2 == 0)
+            {
+                var c = new AboutModel();
+                c.saySomething();
+            }
+            return View();
+        }
+    }
+}
---
+++ MVC2App/Models/AboutModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC2App.Models
+{
+    public class AboutModel
+    {
+        public void saySomething()
+        {
+            try
+            {
+                sayNothing();
+            }
+            catch (Exception e)
+            {
+                throw new NotFiniteNumberException("not finite number", e);
+            }
+        }
+
+        private void sayNothing()
+        {
+            throw new DivideByZeroException();
+        }
+    }
+}
